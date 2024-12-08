27fed99ce72bc6c6226eadea2baa8a125dfd07d9
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-12-08 19:35:12 +0530

    Activity 2

---
+++ Activity 2.py
@@ -0,0 +1,20 @@
+class person(object):
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age  = age
+
+    def info (self):
+        print(self.name)
+        print(self.age)
+
+class employ(person):
+
+    def __init__(self, name, age , address):
+       self.address = address
+
+       super(). __init__(name , age)
+
+
+e =employ("Hanzla" , 11 , "Lucknow")
+e.info()
